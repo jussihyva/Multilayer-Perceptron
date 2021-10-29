db6540bd164a2cfc15778006d5763b23d011500c
Juhani Kauppi <jkauppi@student.hive.fi>
2021-10-29 17:49:13 +0300

    scatter_matrix and Histogram ready.

--- Python/MyHistogram.py
+++ Python/MyHistogram.py
@@ -6,7 +6,7 @@
 #    By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/10/29 14:40:42 by jkauppi           #+#    #+#              #
-#    Updated: 2021/10/29 15:09:33 by jkauppi          ###   ########.fr        #
+#    Updated: 2021/10/29 17:32:46 by jkauppi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,7 @@ class MyHistogram():
 	def __init__(self):
 		pass
 
-	def histogram(self, dataFrame, columnList, axes):
-		legends = ["M", "B"]
+	def histogram(self, dataFrame, columnList, axes, legends):
 		axes = axes.flatten()
 		for i in range(0, len(columnList)):
 			for legend in legends:
@@ -23,4 +22,4 @@ class MyHistogram():
 				axes[i].hist(dataFrame[columnList[i]][legendIndexList],
 				alpha=0.3)
 			axes[i].set_title(columnList[i], fontsize=30)
-			axes[i].legend(legends, fontsize=20)
+			axes[i].legend(legends, fontsize=10)
