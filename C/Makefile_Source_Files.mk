3cc82d74a8519291b4defb9619011e4babdc3bf5
Juhani Kauppi <jkauppi@student.hive.fi>
2021-11-03 15:57:48 +0200

    Makefile finalized in C folder.

--- C/Makefile_Source_Files.mk
+++ C/Makefile_Source_Files.mk
@@ -6,19 +6,19 @@
 #    By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/11/03 11:06:20 by jkauppi           #+#    #+#              #
-#    Updated: 2021/11/03 11:40:27 by jkauppi          ###   ########.fr        #
+#    Updated: 2021/11/03 15:14:23 by jkauppi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 SRC_C_FILES				=
 SRC_H_FILES				=
-LOCAL_LIBS				=	libft_addons.a libftprintf.a libft.a
+LOCAL_LIBS				=
 GLOBAL_LIBS				=
 GLOBAL_LIB_FOLDERS		=
 
 ifeq ($(OS), Darwin)
 	GLOBAL_LIBS			+=
-	GLOBAL_LIB_FOLDERS	+=	$(HOME)/.brew/Cellar/openssl@1.1/1.1.1k/lib
+	GLOBAL_LIB_FOLDERS	+=
 else
 	GLOBAL_LIBS			+=
 endif
