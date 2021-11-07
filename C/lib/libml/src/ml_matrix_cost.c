a80b7c7d05a59b377eaa49cbdd88a4f88e11e337
Juhani Kauppi <jkauppi@student.hive.fi>
2021-11-07 14:43:25 +0200

    ml_matrix_cost function implemented.

--- C/lib/libml/src/ml_matrix_cost.c
+++ C/lib/libml/src/ml_matrix_cost.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/07 12:56:31 by jkauppi           #+#    #+#             */
-/*   Updated: 2021/11/07 13:44:09 by jkauppi          ###   ########.fr       */
+/*   Updated: 2021/11/07 14:40:13 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,24 @@ void	ml_matrix_cost(
 					const t_matrix *const y_hat,
 					t_vector *const cost_vector)
 {
-	(void)y;
-	(void)y_hat;
-	(void)cost_vector;
+	t_size_2d	size;
+	double		*data;
+	double		value;
+
+	data = (double *)y->data;
+	size.rows = -1;
+	while (++size.rows < y_hat->size.rows)
+	{
+		size.cols = -1;
+		while (++size.cols < y_hat->size.cols)
+		{
+			if (data[size.rows] == 1)
+				value = ((double **)y_hat->table)[size.rows][size.cols];
+			else
+				value = 1 - ((double **)y_hat->table)[size.rows][size.cols];
+			((double *)cost_vector->data)[size.rows] += -log(value);
+		}
+		((double *)cost_vector->data)[size.rows] /= y_hat->size.cols;
+	}
 	return ;
 }
--- C/src/training.c
+++ C/src/training.c
@@ -6,7 +6,7 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 15:23:11 by jkauppi           #+#    #+#             */
-/*   Updated: 2021/11/07 13:56:01 by jkauppi          ###   ########.fr       */
+/*   Updated: 2021/11/07 14:42:25 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,14 @@
 
 int	main(void)
 {
-	size_t			i;
-	size_t			max;
 	t_size_2d		size;
 	t_vector		*y;
 	t_matrix		*z;
 	t_matrix		*y_hat;
 	t_vector		*cost;
 
-	max = 1000;
-	max = 10;
 	size.rows = 10;
 	size.cols = 5;
-	i = -1;
 	y = ml_vector_create(size.rows);
 	ml_vector_rand(y, 0, 2);
 	ml_vector_print("Y", y);
@@ -36,12 +31,10 @@ int	main(void)
 	y_hat = ml_matrix_create(size.rows, size.cols);
 	ml_matrix_rand(z, -10, 10);
 	cost = ml_vector_create(size.rows);
-	while (++i < max)
-	{
-		ml_sigmoid(z, y_hat);
-		ml_matrix_cost(y, y_hat, cost);
-	}
+	ml_sigmoid(z, y_hat);
+	ml_matrix_cost(y, y_hat, cost);
 	ml_matrix_print("Y hat", y_hat);
+	ml_vector_print("Cost", cost);
 	ml_matrix_remove(&z);
 	ml_matrix_remove(&y_hat);
 	return (0);
